3bb2b18a86dd345fb2b78d53e01c62ee43ffa07c
Hamza Ghani <hamza.ghani@utexas.edu>
2017-05-18 02:41:00 -0500

    basic syntax for delcaring stuff in go

--- hello.go
+++ hello.go
@@ -11,22 +11,36 @@ import (
 */
 var c, python bool
 
-//but we can omit types if we initliaze
-//keeping is type and initializing also fine
+/* but we can omit types if we initliaze
+* keeping is type and initializing also fine
+*/
 var numA, numB, cow = 1, 2, "moo"
+
+//or we can factor variables
+var (
+  tf  = true
+  num = 15
+  z complex128 = -5+4i
+
+)
+
+//constant
+const e = 2.7
+
+//factor version of const 
+const (
+  pi = 3.14
+  tau = 6.28
+
+)
 
 func main() {
     var hi string = "hello"
-
     //:= only used for new variables, and inside functions
     //and the type becomes that of which its first declared to
     k:= "yo"
-    k = "world"
+    k = "rewrite"
 
-
-    fmt.Println(k)
-    fmt.Println(multDiv(8))
-    fmt.Println(hi, k, "\nhere are numbers", numA, numB, "\nthe cow goes", cow)
 
 }
 
@@ -39,4 +53,21 @@ func multDiv(a int) (x, y int){
   y = a*2
   return
 
+}
+
+/*This is doesnt compile
+* b/c y:= isnt new, its basically a local variable
+* defined at the top in the return portion
+func convert() (y float64) {
+  x := 2
+  y := float64(x)
+  return
+}
+*/
+
+//converting data types
+func convert() (y float64) {
+  x := 2
+  y = float64(x)
+  return
 }
