6ce15b0f573e2a33e5b19591346f7863a311bed7
Hamza Ghani <hamza.ghani@utexas.edu>
2017-05-17 22:15:14 -0500

    like c with new features

--- hello.go
+++ hello.go
@@ -6,9 +6,27 @@ import (
 
 )
 
+/* have to put var when declaring variables
+* outside of function parameters
+*/
+var c, python bool
+
+//but we can omit types if we initliaze
+//keeping is type and initializing also fine
+var numA, numB, cow = 1, 2, "moo"
 
 func main() {
+    var hi string = "hello"
+
+    //:= only used for new variables, and inside functions
+    //and the type becomes that of which its first declared to
+    k:= "yo"
+    k = "world"
+
+
+    fmt.Println(k)
     fmt.Println(multDiv(8))
+    fmt.Println(hi, k, "\nhere are numbers", numA, numB, "\nthe cow goes", cow)
 
 }
 
